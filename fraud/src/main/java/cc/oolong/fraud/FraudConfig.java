da4da4bcaff211eba0a4dd3db74dbfdb2de10070
mingfai <mingfai@live.com>
2023-05-11 23:44:44 +0800

    zipkin distributed tracing

--- fraud/src/main/java/cc/oolong/fraud/FraudConfig.java
+++ fraud/src/main/java/cc/oolong/fraud/FraudConfig.java
@@ -1,22 +1,13 @@
-package cc.oolong.customer;
+package cc.oolong.fraud;
 
 import feign.Capability;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class CustomerConfig {
-//   @Bean
-//   @LoadBalanced
-//    public RestTemplate restTemplate() {
-//       return new RestTemplate();
-//   }
-
-
+public class FraudConfig {
     @Bean
     public Capability capability(final MeterRegistry registry) {
         return new MicrometerCapability(registry);
