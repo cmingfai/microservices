cd702eec9d0c239632299cd493f3a263855edf9e
mingfai <mingfai@live.com>
2023-05-12 15:03:30 +0800

    api gateway

---
+++ api-gateway/src/main/java/cc/oolong/apigateway/ApiGatewayConfig.java
@@ -0,0 +1,14 @@
+package cc.oolong.apigateway;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class ApiGatewayConfig {
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
+}
