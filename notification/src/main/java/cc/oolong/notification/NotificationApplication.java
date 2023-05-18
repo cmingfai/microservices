dcad07d69c7969fcb34be3f9dab7e69d8bf065e4
mingfai <mingfai@live.com>
2023-05-18 12:27:35 +0800

    Refactoring Microservices for k8s

--- notification/src/main/java/cc/oolong/notification/NotificationApplication.java
+++ notification/src/main/java/cc/oolong/notification/NotificationApplication.java
@@ -7,11 +7,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {"cc.oolong.amqp","cc.oolong.notification"}
 )
 @EnableDiscoveryClient
+@PropertySources({
+        @PropertySource("clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class,args);
